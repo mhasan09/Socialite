ea3894b99da9913a235058bd07ae956e49741c50
Mahabubul Hasan <shourav6688@gmail.com>
2019-06-14 00:45:11 +0600

    added templating engine

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const expressEdge = require('express-edge')
 app.use(express.static('public'))
+app.use(expressEdge) //adding functionality for templating engine
+app.set('views', `${__dirname}/views`)
 app.listen(3000,()=>{
     console.log("app listening on port 3000")
 })
 app.get('/',(request,response)=>{
-    response.sendFile(path.resolve(__dirname,'pages/index.html'))
+    response.render('index')
 })
 app.get('/about',(request,response)=>{
     response.sendFile(path.resolve(__dirname,'pages/about.html'))
