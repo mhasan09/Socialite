5ee8d9bf0daf2a55da47669b41228efaae631d1e
Mahabubul Hasan <shourav6688@gmail.com>
2019-06-14 00:09:50 +0600

    added UI,directory changes

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.listen(3000,()=>{
     console.log("app listening on port 3000")
 })
 app.get('/',(request,response)=>{
-    response.sendFile(path.resolve(__dirname,'index.html'))
+    response.sendFile(path.resolve(__dirname,'pages/index.html'))
 })
 app.get('/about',(request,response)=>{
-    response.sendFile(path.resolve(__dirname,'about.html'))
+    response.sendFile(path.resolve(__dirname,'pages/about.html'))
 })
 app.get('/contact',(request,response)=>{
-    response.sendFile(path.resolve(__dirname,'contact.html'))
+    response.sendFile(path.resolve(__dirname,'pages/contact.html'))
 })
