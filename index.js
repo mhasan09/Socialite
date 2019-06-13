4fe061f06a55f9712cbeebeab8eaafd4f925ec19
Mahabubul Hasan <shourav6688@gmail.com>
2019-06-13 20:04:55 +0600

    added some paths

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 app.listen(3000,()=>{
     console.log("app listening on port 3000")
 })
 app.get('/',(request,response)=>{
-    response.json({
-        name : 'kati franz'
-    })
+    response.sendFile(path.resolve(__dirname,'index.html'))
 })
 app.get('/about',(request,response)=>{
-    response.send({
-        name : "Mahabubul Hasan"
-    })
+    response.sendFile(path.resolve(__dirname,'about.html'))
+})
+app.get('/contact',(request,response)=>{
+    response.sendFile(path.resolve(__dirname,'contact.html'))
 })
