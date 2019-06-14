449e441812dd09457ce6e3ef6b8e46c18ee80d04
Mahabubul Hasan <shourav6688@gmail.com>
2019-06-14 14:05:36 +0600

    testing again

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose')
 const Post = require('./database/models/post')
 mongoose.connect('mongodb://localhost/socialite')
-Post.create({
-    title : 'sup Mr White',
-    description : 'so',
-    content : 'ada'
+// Post.create({
+//     title : 'sup Jesse',
+//     description : 'Tuco',
+//     content : 'Gustavo and mike'
 
-},(error,post)=>{
-    console.log(error,post)
+// },(error,post)=>{
+//     console.log(error,post)
+// })
+
+Post.find({},(error,posts)=>{
+    console.log(error,posts)
 })
