25e956ee023f1ead06ab794e6da463c304bcf050
Mahabubul Hasan <shourav6688@gmail.com>
2019-06-14 14:32:34 +0600

    added findbyID

--- test.js
+++ test.js
@@ -10,6 +10,9 @@ mongoose.connect('mongodb://localhost/socialite')
 //     console.log(error,post)
 // })
 
-Post.find({},(error,posts)=>{
-    console.log(error,posts)
+// Post.find({},(error,posts)=>{
+//     console.log(error,posts)
+// })
+Post.findById("5d0353be1a1be22414b22e4",(error,post)=>{
+    console.log(error, post)
 })
