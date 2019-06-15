22dfe30052d67214a0e4b23900e49f04702d9235
Mahabubul Hasan <shourav6688@gmail.com>
2019-06-15 13:47:26 +0600

    revert to previous version

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
-const express = require('express')
 const path = require('path')
-const app = express()
 const expressEdge = require('express-edge')
+const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const Post = require('./database/models/post')
-mongoose.connect('mongodb://localhost/socialite', { useNewUrlParser: true })
+
+const Post = require('./database/models/Post')
+
+const app = new express()
+
+mongoose.connect('mongodb://localhost/node-js-blog')
+
 app.use(express.static('public'))
-app.use(expressEdge) //adding functionality for templating engine
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended : true}))
+app.use(expressEdge)
 app.set('views', `${__dirname}/views`)
-app.listen(3000,()=>{
-    console.log("app listening on port 3000")
-})
-app.get('/',async(request,response)=>{
-    const posts = await Post.find({})
-    console.log(posts)
-    response.render('index',{
-        posts  
-    })
-})
-app.get('/about',(request,response)=>{
-    response.render('about')
-})
-app.get('/contact',(request,response)=>{
-    response.render('contact')
-})
-app.get('/post/:id',async(request,response)=>{
-    const post = await Post.findById(request.params.id)
-    response.render('post',{
-        post
-    })
-})
-app.get('/post/new',(request,response)=>{
-    response.render('create')
-})
-app.post('/post/store',(request,response)=>{
-    Post.create(request.body,(error,post)=>{
-    response.redirect('/')
-    }) 
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  console.log(posts)
+  res.render('index', {
+    posts
+  })
 })
 
+app.get('/posts/new', (req, res) => {
+  res.render('create')
+})
 
+app.post('/posts/store', (req, res) => {
+  Post.create(req.body, (error, post) => {
+    res.redirect('/')
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/post/:id', async (req, res) => {
+  const post = await Post.findById(req.params.id)
+  res.render('post', {
+    post
+  })
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+app.listen(4000, () => {
+  console.log('App listening on port 4000')
+})
