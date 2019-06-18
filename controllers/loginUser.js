e34390cb64db80a1e6858c08bd0dd3fbbd64f838
Mahabubul Hasan <shourav6688@gmail.com>
2019-06-18 13:12:02 +0600

    bcrypt login process

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,4 +1,19 @@
 const User = require('../database/models/User')
+const bcrypt = require('bcrypt')
 module.exports = (req,res)=> {
-    res.redirect('/')
+    const {email,password} = req.body
+    User.findOne({email}, (error,user)=> {
+        if(user){
+            bcrypt.compare(password , user.password, (error,same)=>{
+                if (same){
+                    res.redirect('/')
+                } else {
+                    res.redirect('/auth/login')
+                }
+            })
+        } else {
+            return res.redirect('/auth/login')
+        }
+    })
+   
 }
