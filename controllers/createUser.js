03f0c0e805065e33f5622247d66d9dc332f224d9
Mahabubul Hasan <shourav6688@gmail.com>
2019-06-18 18:36:24 +0600

    display validation errors in template

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,4 +1,6 @@
 module.exports = (req,res) => {
     console.log(req.session.registrationErrors)
-    res.render('register')
+    res.render('register', {
+        errors : req.session.registrationErrors
+    })
 }
